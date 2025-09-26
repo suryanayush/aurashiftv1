df8b08a19703a79b89ec77bb999f0ae14aa63d29
Kapil Patel <kapil@aag.ventures>
2025-09-26 14:59:55 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import './global.css';
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <ScreenContent title="bbjb" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
